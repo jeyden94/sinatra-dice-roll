e2b328e24aad3b770a8e6a729fafe740cc94a33c
jeyden94 <jeyden94@gmail.com>
2024-10-21 17:53:28 +0000

    Setup with Sinatra, puma, and rackup

---
+++ dice.rb
@@ -0,0 +1,16 @@
+require "sinatra"
+
+get("/") do
+  "Hello, world!"
+end
+
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+
+require "sinatra/reloader"
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
