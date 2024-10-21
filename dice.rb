dc153b254b2437665c592a32d38525eb381c45c6
jeyden94 <jeyden94@gmail.com>
2024-10-21 22:18:01 +0000

    V1 complete!

--- dice.rb
+++ dice.rb
@@ -20,44 +20,105 @@ get("/-") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
 end
 
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
+end
+
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
   erb(:two_six)
 end
 
-get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+# get("/dice/2/6") do
+#   first_die = rand(1..6)
+#   second_die = rand(1..6)
+#   sum = first_die + second_die
+	
+#   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+#   erb(:two_six)
+# end
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+get("/dice/2/10") do
+  @rolls = []
+
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
 
   erb(:two_ten)
 end
 
-get("/dice/1/20") do
-  die = rand(1..20)
+# get("/dice/2/10") do
+#   first_die = rand(1..10)
+#   second_die = rand(1..10)
+#   sum = first_die + second_die
 
-  @outcome = "You rolled a #{die}."
+#   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+#   erb(:two_ten)
+# end
+
+
+get("/dice/1/20") do
+  @rolls = []
+
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
 
   erb(:one_twenty)
 end
 
+# get("/dice/1/20") do
+#   @die = rand(1..20)
+
+#   @outcome = "You rolled a #{@die}."
+
+#   erb(:one_twenty)
+# end
+
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
+  @rolls = []
 
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
+  5.times do
+    die = rand(1..4)
 
-  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}"
+    @rolls.push(die)
+  end
 
   erb(:five_four)
 end
+
+# get("/dice/5/4") do
+#   first_die = rand(1..4)
+#   second_die = rand(1..4)
+#   third_die = rand(1..4)
+#   fourth_die = rand(1..4)
+#   fifth_die = rand(1..4)
+
+#   sum = first_die + second_die + third_die + fourth_die + fifth_die
+
+#   @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}"
+
+#   erb(:five_four)
+# end
